279222f31810250445d28498ac0f7786c4ceef79
vijithahmbandara <vijithahmbandara@gmail.com>
2023-09-05 17:05:42 +0530

    Save a new Book with mongoose

--- BackEnd/config.js
+++ BackEnd/config.js
@@ -1,3 +1,3 @@
-export const PORT = 4000;
+export const PORT = 65000;
 
 export const mongoDBURL ='mongodb+srv://root:VI12JI34th5a@book-store-mern.rkuslhc.mongodb.net/book-collection?retryWrites=true&w=majority'
