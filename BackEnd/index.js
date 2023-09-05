fabe51c9fb44cb42d55fba275add62d92f13cff1
vijithahmbandara <vijithahmbandara@gmail.com>
2023-09-05 11:32:45 +0530

    Create Node.js project from scratch

--- BackEnd/index.js
+++ BackEnd/index.js
@@ -1,1 +1,8 @@
-import Express from "express";
+import express from "express";
+import {PORT} from "./config.js";
+
+const app = express();
+
+app.listen(PORT, () =>{
+    console.log(`App is listening to port: ${PORT}`);
+});
